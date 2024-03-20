298e85785d9f9fb925917342d7a15b7520ab5845
rhenoresky <rheno2002@gmail.com>
2024-03-20 20:48:55 +0700

    add hero component

--- src/scripts/components/hero.js
+++ src/scripts/components/hero.js
@@ -6,10 +6,12 @@ class Hero extends HTMLElement {
   render() {
     this.innerHTML = `
       <section class="hero">
-        <div class="hero-inner">
-          <h1 class="hero-title">Selamat datang di webiste delicious</h1>
-          <p class="hero-tagline">
-            Jelajahi semua restoran yang ada untuk menemukan pengalaman menyantap makanan dengan rasa yang khas dan lezat serta pelayanan yang memuaskan
+        <div class="hero__inner">
+          <h1 class="hero__title">Selamat datang di webiste delicious</h1>
+          <p class="hero__tagline">
+            Jelajahi semua restoran yang ada untuk menemukan pengalaman
+            menyantap makanan dengan rasa yang khas dan lezat serta pelayanan
+            yang memuaskan
           </p>
         </div>
       </section>
@@ -17,4 +19,4 @@ class Hero extends HTMLElement {
   }
 }
 
-customElements.define("hero-component", Hero);
+customElements.define('hero-component', Hero);
