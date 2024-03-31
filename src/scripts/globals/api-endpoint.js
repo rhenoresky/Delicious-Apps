9666a9568e63399b117cd5689e193ab7d4b68e05
rhenoresky <rheno2002@gmail.com>
2024-03-31 20:47:42 +0700

    add api endpoint

---
+++ src/scripts/globals/api-endpoint.js
@@ -0,0 +1,11 @@
+import CONFIG from './config';
+
+const API_ENDPOINT = {
+  LIST_RESTAURANT: `${CONFIG.BASE_URL}/list`,
+  DETAIL: (id) => `${CONFIG.BASE_URL}/detail/${id}`,
+  SEARCH: (query) => `${CONFIG.BASE_URL}/search?q=${query}`,
+  ADD_REVIEW: `${CONFIG.BASE_URL}/review`,
+  GET_IMAGE: ({size, id}) => `${CONFIG.BASE_URL}/images/${size}/${id}`,
+};
+
+export default API_ENDPOINT;
