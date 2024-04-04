c3c09818e05adcde71575df748768c292d76c006
rhenoresky <rheno2002@gmail.com>
2024-04-04 19:58:13 +0700

    add page detail

---
+++ src/scripts/views/pages/detail.js
@@ -0,0 +1,55 @@
+import UrlParser from '../../routes/url-parser';
+import TheRestaurantDbSource from '../../data/restaurant-source';
+import LikeButtonInitiator from '../../utils/like-button-initiator';
+import restaurantDetail from '../templates/detail-restaurant';
+import {loading} from '../templates/loading';
+import {swalError, swalSuccess} from '../../utils/swal';
+
+const Detail = {
+  async render() {
+    return `
+      <div id="restaurant"></div>
+      <div id="likeButtonContainer"></div>
+    `;
+  },
+
+  async afterRender() {
+    const url = UrlParser.parseActiveUrlWithoutCombiner();
+    const restaurantContainer = document.querySelector('#restaurant');
+    restaurantContainer.innerHTML = '';
+    restaurantContainer.innerHTML = loading();
+
+    try {
+      const restaurant = await TheRestaurantDbSource.detailRestaurant(url.id);
+      restaurantContainer.innerHTML = '';
+      restaurantContainer.innerHTML = restaurantDetail(restaurant);
+
+      const formReview = document.querySelector('#form-review');
+      const inputName = document.querySelector('#name-input');
+      const inputReview = document.querySelector('#review-input');
+
+      formReview.addEventListener('submit', async (e) => {
+        e.preventDefault();
+
+        await TheRestaurantDbSource.addReview({
+          id: url.id,
+          name: inputName.value,
+          review: inputReview.value,
+        });
+
+        await this.afterRender();
+        swalSuccess('Successfully added reviews');
+      });
+
+      await LikeButtonInitiator.init({
+        likeButtonContainer: document.querySelector('#likeButtonContainer'),
+        restaurant,
+      });
+    } catch (err) {
+      restaurantContainer.innerHTML = '';
+      swalError(err.message);
+    }
+  },
+};
+
+export default Detail;
