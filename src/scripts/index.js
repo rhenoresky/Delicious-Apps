6bed3b0d60832d7e9125e314588409357a2d6382
rhenoresky <rheno2002@gmail.com>
2024-04-27 20:45:08 +0700

    add lazy load

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,13 +7,23 @@ import '../styles/detail.css';
 import '../styles/loading.css';
 import '../styles/responsive.css';
 import './components/nav-bar';
+import 'lazysizes';
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 import App from './views/app';
 import swRegister from './utils/sw-register';
 
 const app = new App(document.querySelector('#mainContent'));
 
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+};
+
 window.addEventListener('hashchange', () => {
   app.renderPage();
+  scrollToTop();
 });
 
 window.addEventListener('load', async () => {
