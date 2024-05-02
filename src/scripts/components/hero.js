d62c70ce104df03dab9786a03e2923b3eab7b501
rhenoresky <rheno2002@gmail.com>
2024-05-02 19:55:47 +0700

    refac: using picture tag for hero element

--- src/scripts/components/hero.js
+++ src/scripts/components/hero.js
@@ -6,6 +6,11 @@ class Hero extends HTMLElement {
   render() {
     this.innerHTML = `
       <section class="hero">
+        <picture class="hero__image">
+          <source media="(max-width: 440px)" srcset="./images/heros-responsive/hero-image-small.jpg" />
+          <source media="(max-width: 780px)" srcset="./images/heros-responsive/hero-image-large.jpg" />
+          <img src="./images/heros-responsive/hero-image-extra-large.jpg" alt="Hero image" />
+        </picture>
         <div class="hero__inner">
           <h1 class="hero__title">Selamat datang di webiste delicious</h1>
           <p class="hero__tagline">
